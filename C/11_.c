755cafbe71c4391b03e1195be9607a02887aa970
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-04-26 23:04:06 -0600

    C code

---
+++ C/11_.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main () {
+    float x, s, t, num = 1.00, fac = 1.00;
+    int i, n, pr, y = 2, m = 1;
+
+    printf("Ingresa un valor x: ");
+    scanf("%d", &x);
+
+    printf("Ingresa un numero de terminos: ");
+    scanf("%d", &n);
+
+    s = 1.00;
+    t = 1.00;
+
+    for(i = 1; i < n; i++){
+        
+    }
+}
