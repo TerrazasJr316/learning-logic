a6e3810c15f08c31a853a3fb50576e803dd1182f
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-05-04 23:13:41 -0600

    C code

--- C/12_.c
+++ C/12_.c
@@ -5,4 +5,8 @@ int noDigitos(int n1);
 int main () {
 
     int n1, ctr;
+
+    printf("\n\n contar los digitos que se obtienen: \n");
+    printf("-----------------------------------------\n");
+    
 }
