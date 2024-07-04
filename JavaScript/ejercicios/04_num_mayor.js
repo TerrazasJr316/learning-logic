415887d5131f96c137f3ea9c8416f6cd375cf477
Josue Terrazas Mendoza <160078635+TerrazasJr316@users.noreply.github.com>
2024-07-04 05:59:29 +0000

    git add JavaScript/ejercicios/03_par_impar.js git add JavaScript/ejercicios/04_num_mayor.js
    git commit -m ""

---
+++ JavaScript/ejercicios/04_num_mayor.js
@@ -0,0 +1,12 @@
+//Escribe una función que retorne el numero mayor entre a y b
+
+function mayor(a, b){
+    if (a > b) {
+        return a
+    } else {
+        return b
+    }
+}
+
+console.log(mayor(5, 4));
+console.log(mayor(7, 9));
