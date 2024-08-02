d6c88f20db522f40626335d954bf60d163be0ff8
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-08-02 17:40:49 -0600

    Java code

---
+++ Java/HeapSortUsingSTL.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class HeapSortUsingSTL {
+
+    public static void headSort(int[] arr) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i = 0; i < arr.length; i++){
+            maxHeap.offer(arr[i]);
+        }
+        for (int i = arr.length - 1; i >= 0; i--){
+            arr[i] = maxHeap.poll();
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {60, 20, 40, 70, 30, 10};
+        System.out.println("Before Sorting: " + Arrays.toString(arr));
+        headSort(arr);
+        System.out.println("After Sorting: " + Arrays.toString(arr));
+    }
+}
