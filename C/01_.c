5e2c65a09eb10eb97d90222622fcf8ed151fc8c9
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-08-07 23:33:50 -0600

    C code

---
+++ C/01_.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    int i =1, n;
+    printf("Ingresa un numero: ");
+    scanf("%d", &n);
+
+    while(i<=n) {
+        print("%d \n", i);
+        i++;
+    }
+}
