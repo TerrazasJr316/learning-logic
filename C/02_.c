01d751325d48512dbe63e819668850c2eefb4923
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-08-14 21:50:44 -0600

    C code

---
+++ C/02_.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    int i =1, n;
+    
+    printf("Ingresa un numero: ");
+    scanf("%d", &n);
+
+    for(i=1; i <= 10; i++){
+        print("%d \n", n * i);
+    }
+}
