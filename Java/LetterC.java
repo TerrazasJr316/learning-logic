7c33913abf55a7e2786a7d88c8ec012560fdd0e6
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-09-03 20:14:25 -0600

    Java code

--- Java/LetterC.java
+++ Java/LetterC.java
@@ -2,7 +2,7 @@ public class LetterC {
     public static void main(String[] args) {
         for(int i=1; i<=7; i++) {
             for(int j=1; j<=7; j++){
-                if((i==1 && j >1) || (j == 1 && i>1 && i<7) || (i ==7 && j >1)){
+                if((i==1 && j >1) || (j == 1 && i>1 && i<7) || (i ==7 && j >1)) {
                     System.out.print("*");
                 }
                 else{
