8c344af3d022b2cd6ebb5c302e8afbf99a830140
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-09-29 21:53:39 -0600

    python code

---
+++ Python/O.py
@@ -0,0 +1,16 @@
+print("-------------------------------------------------------")
+print("Ejercicio8: CALCULAR PERÍMETRO Y SUPERFICIE DEL RECTÁNGULO")
+print("-------------------------------------------------------")
+
+#Entradas
+print("Ingrese Base y Alto: ")
+BASE = float( input("Base: "))
+ALTO = float( input("Alto: "))
+SUP = BASE*ALTO
+PER = 2*(BASE + ALTO)
+
+#Salida
+print("\nSALIDA: ")
+print("-------------------------------------------------------")
+print("Superficie:", SUP)
+print("Perímetro:", PER)
