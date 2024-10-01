227ff73b77d5ff4ab7376bf6f8c75e1c7edc972a
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-09-30 21:58:36 -0600

    python code

--- Python/R.py
+++ Python/R.py
@@ -1,17 +1,16 @@
 print("-------------------------------------------------------")
-print("Complemento1: CALCULAR VOLUMEN DE LA ESFERA.")
+print("Complemento2: PERÍMETRO TRIANGULO EQUILÁTERO.")
 print("-------------------------------------------------------")
 
-#Constantes
-PI = 3.1416
-
 #Entradas
-r = float( input("Ingrese Radio: "))
+print("Ingrese altura del triángulo: ")
+h = float( input())
 
 #Proceso
-v = 4/3 * PI * r**3
+p = 3*(2*h)/3**0.5
 
 #Salida
 print("\nSALIDA: ")
 print("-------------------------------------------------------")
-print("El volumen de la esfera es:", v)
+print("El perímetro del triángulo será:", p)
+
