59532031441f1ac2d90ca4818b232d6f8b6d9616
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-10-03 22:01:58 -0600

    python code

--- Python/V.py
+++ Python/V.py
@@ -1,13 +1,17 @@
 print("-------------------------------------------------------")
 print("Complemento6: Millas a Kilómetros.")
 print("-------------------------------------------------------")
+
 #Constantes
 MI = 1.609344
+
 #Entradas
 print("Ingrese número de millas:")
 m = float( input())
+
 #Proceso
 k = m*MI
+
 #Salida
 print("\nSALIDA: ")
 print("-------------------------------------------------------")
