869397c0071bd776025f8b79f7e02788a1eee587
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-10-03 22:01:26 -0600

    python code

--- Python/T.py
+++ Python/T.py
@@ -1,13 +1,16 @@
 print("-------------------------------------------------------")
 print("Complemento4: CALCULAR LA VELOCIDAD DE UN MÓVIL.")
 print("-------------------------------------------------------")
+
 #Entradas
 print("Ingrese el tiempo en segundos:")
 t = float( input())
 print("Ingrese la distancia en metros:")
 d = float( input())
+
 #Proceso
 v = d/t
+
 #Salida
 print("\nSALIDA: ")
 print("-------------------------------------------------------")
