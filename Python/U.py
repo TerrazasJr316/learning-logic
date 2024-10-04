0bca08c05d3f88f17c5c18ccd5730c272dd2697d
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-10-03 22:00:55 -0600

    python code

---
+++ Python/U.py
@@ -0,0 +1,14 @@
+print("-------------------------------------------------------")
+print("Complemento5: CALCULAR MONTO A PAGAR.")
+print("-------------------------------------------------------")
+#Entradas
+print("Ingrese costo unitario del artículo:")
+c = float( input())
+print("Ingrese el número de docenas:")
+d = int( input())
+#Proceso
+p = d*12 * c
+#Salida
+print("\nSALIDA: ")
+print("-------------------------------------------------------")
+print("El precio del artículo es:", p)
