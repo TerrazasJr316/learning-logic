37cc8dc78abf45f094eeb05f5fac9e0959d3ee88
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-10-04 22:04:18 -0600

    python code

---
+++ Python/Y.py
@@ -0,0 +1,16 @@
+print("-------------------------------------------------------")
+print("Complemento9: RADIANES a SEXAGESIMALES y CENTESIMALES")
+print("-------------------------------------------------------")
+#Constantes
+PI = 3.1416
+#Entradas
+print("Ingrese ángulo en radianes:")
+x = float( input())
+sex = x*180/PI
+cen = x*200/PI
+#Proceso
+#Salida
+print("\nSALIDA: ")
+print("-------------------------------------------------------")
+print("En sexagesimales es:", sex)
+print("En centesimales es:", cen)
