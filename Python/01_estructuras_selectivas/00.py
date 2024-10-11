d75ee8e663a203e0817e2db64104dd8adf6b8428
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-10-11 14:26:43 -0600

    python code

---
+++ Python/01_estructuras_selectivas/00.py
@@ -0,0 +1,18 @@
+#Decoración: Nombre del Algoritmo
+print("-------------------------------------------------------")
+print("EVALUAR SI NUMERO MENOR A 100.")
+print("-------------------------------------------------------")
+
+#Entradas
+print("Ingrese un número: ")
+x = int( input())
+
+#Proceso
+if x < 100 :
+    print("Número Pequeño")
+
+""""
+NOTA: para inidicar que tdodo esta dentro
+de la estructura IF hay que hacer uso de la
+indentación con ayuda de la tecla TABULADOR
+"""
