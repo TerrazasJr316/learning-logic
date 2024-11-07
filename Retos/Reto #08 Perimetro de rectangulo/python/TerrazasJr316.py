81f12043599f1aa96ee3992d4850cc6b1f2d67fa
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-11-07 07:57:23 -0600

    Reto #8 - python

---
+++ Retos/Reto #08 Perimetro de rectangulo/python/TerrazasJr316.py
@@ -0,0 +1,9 @@
+print("Ingrese Base y Alto: ")
+BASE = float( input("Base: "))
+ALTO = float( input("Alto: "))
+
+SUP = BASE*ALTO
+PER = 2*(BASE + ALTO)
+
+print("Superficie:", SUP)
+print("Perímetro:", PER)
