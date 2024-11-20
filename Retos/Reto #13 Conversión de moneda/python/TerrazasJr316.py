8231b8fe1bfcd547fd1c1299c356bb1efcd5ddcc
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-11-20 08:04:33 -0600

    Reto #13 - python

---
+++ Retos/Reto #13 Conversión de moneda/python/TerrazasJr316.py
@@ -0,0 +1,11 @@
+EU = 3.84
+DO = 2.28
+
+print("Ingrese la cantidad de soles:")
+s = float( input())
+
+d = s/DO
+e = s/EU
+
+print("En", s, "soles hay ", e, "euros")
+print("En", s, "soles hay ", d, "dólares")
