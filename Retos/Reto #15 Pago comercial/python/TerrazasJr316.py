4efcb8b73d923623b97b73346dbd03f8559010a5
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-11-23 08:06:46 -0600

    Reto #15 - python

---
+++ Retos/Reto #15 Pago comercial/python/TerrazasJr316.py
@@ -0,0 +1,8 @@
+print("Ingrese costo unitario del artículo:")
+c = float( input())
+print("Ingrese el número de docenas:")
+d = int( input())
+
+p = d*12 * c
+
+print("El precio del artículo es:", p)
