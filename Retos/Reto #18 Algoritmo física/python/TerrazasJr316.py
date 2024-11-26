d435bae42fa49597b522c1deffa8bc6a2b41ddb1
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-11-26 08:11:37 -0600

    Reto #18 - python

---
+++ Retos/Reto #18 Algoritmo física/python/TerrazasJr316.py
@@ -0,0 +1,10 @@
+print("Ingrese las velocidades:")
+va = float( input("Va: "))
+vb = float( input("Vb: "))
+print("Ingrese la distancia que los separa:")
+
+D = float( input())
+
+te = D/(va+vb)
+
+print("Los cuerpos se encontraran en:", te, "segundos")
