f6e9221c688633a1a38368e9266124b45175cbd2
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-12-04 08:23:16 -0600

    Reto #26 - python

---
+++ Retos/Reto #26 Por o impar entre 0 - 10/python/TerrazasJr316.py
@@ -0,0 +1,17 @@
+NUM = int( input("Ingrese número:" ))
+#Proceso
+#Usando un diccionario
+par_Impar = {
+1 : 'Impar',
+3 : 'Impar',
+5 : 'Impar',
+7 : 'Impar',
+9 : 'Impar',
+2 : 'Par',
+4 : 'Par',
+6 : 'Par',
+8 : 'Par',
+10 : 'Par'
+}
+
+print(par_Impar.get(NUM, "Numero fuera de Rango"))
