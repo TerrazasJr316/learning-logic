fa2a3b1075f832bf7cd9b2c44f88148195c43609
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-12-08 08:28:28 -0600

    Reto #29 - python

---
+++ Retos/Reto #29 Ordenamiento de 2 números/python/TerrazasJr316.py
@@ -0,0 +1,8 @@
+print("Ingrese dos números: ")
+a = int( input("Ingrese a: "))
+b = int( input("Ingrese b: "))
+
+if a < b:
+    print("Los números son:", a, b)
+else:
+    print("Los números son:", b, a)
