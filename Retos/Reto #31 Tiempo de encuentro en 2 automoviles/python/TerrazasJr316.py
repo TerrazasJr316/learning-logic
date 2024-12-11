c8cd794c93ba40c9451008d894569076fd502665
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-12-11 08:37:20 -0600

    Reto #31 - python

--- Retos/Reto #31 Tiempo de encuentro en 2 automoviles/python/TerrazasJr316.py
+++ Retos/Reto #31 Tiempo de encuentro en 2 automoviles/python/TerrazasJr316.py
@@ -1,12 +1,13 @@
-print("Ingrese las velocidades de ambos vehículos: ")
-v1 = float( input("V1: "))
-v2 = float( input("V2: "))
-print("Ingrese la distancia que los separa: ")
-d = float( input("Distancia: "))
+print("Ingrese la fecha: ")
+a = int( input("Año: "))
+m = int( input("Mes: "))
+d = int( input("Día: "))
 
-if v1 > 0 and v2 > 0 :
-    t = d/(v1+v2)
-    print(t, "segundos")
+if d > 0 and d < 30 :
+    print("Mañana es:", d+1, m, a)
 else:
-    print("ERROR")
-    
+    if m > 0 and m < 12 :
+        print("Mañana es:", 1, m+1, a)
+    else:
+        print("Mañana es:", 1, 1, a+1)
+
