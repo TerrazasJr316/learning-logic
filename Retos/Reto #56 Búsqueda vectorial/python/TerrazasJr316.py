daba20a01584854851c1e65c57a7e5b7da08d5ff
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2025-01-07 10:00:29 -0600

    Reto #56 - python

--- Retos/Reto #56 Búsqueda vectorial/python/TerrazasJr316.py
+++ Retos/Reto #56 Búsqueda vectorial/python/TerrazasJr316.py
@@ -1,1 +1,20 @@
+valor = []
 
+print ("Ingrese número de elementos del arreglo")
+m = int( input())
+
+print ("Ingrese los elementos del arreglo")
+for i in range(m) :
+    elemento = int( input("Ingrese Elemento: "))
+    valor.append(elemento)
+
+print ("Ingrese el valor buscado: ")
+bus = int( input())
+
+print ("La posición del valor buscado será:")
+
+for i in range(m) :
+    if valor[i] == bus :
+        r = i
+        print ("La posición del elemento es", r+1)
+    break
