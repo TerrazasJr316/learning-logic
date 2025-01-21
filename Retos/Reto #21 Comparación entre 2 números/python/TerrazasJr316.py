54b7c9afaade9f2845296505de8abf9294aaf1aa
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2025-01-21 08:15:38 -0600

    Reto #21 - python

---
+++ Retos/Reto #21 Comparación entre 2 números/python/TerrazasJr316.py
@@ -0,0 +1,8 @@
+print("Ingrese 2 números: ")
+x = int( input("Primer Número: "))
+y = int( input("Segundo Número: "))
+
+if x > y :
+    print("El menor es:", y)
+else:
+    print("El menor es:", x)
